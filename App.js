f25ab22dba29586e0410486d1c6ef7181d76112f
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-12-12 16:39:32 +0200

    delete comments in app

--- App.js
+++ App.js
@@ -5,15 +5,12 @@ import RegisterScreen from "./screens/RegistrationScreen";
 
 LogBox.ignoreLogs(['Remote debugger']);
 
-// https://www.waldo.com/blog/add-an-image-picker-react-native-app
 export default function App() {
   return (
-    // <React.StrictMode>
     <>
       {/* <LoginScreen /> */}
       <RegisterScreen />
       </>
-    // </React.StrictMode>
       
   );
 }
